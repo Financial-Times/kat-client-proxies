64947295b86fd62587f4ca381f8dcb0f1bfe18a9
Dan Murphy <dan.murphy@ft.com>
2017-02-13 22:23:31 +0000

    Add in existential checking for membership API

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ require('isomorphic-fetch');
 const myFTClient = require('./lib/myFTClient');
 const acquisitionCtxClient = require('./lib/acquisitionCtxClient');
 const userProfileClient = require('./lib/userProfileClient');
+const clientErrors = require('./lib/statusErrors');
 
 module.exports={
 	myFTClient,
 	acquisitionCtxClient,
-	userProfileClient
+	userProfileClient,
+	clientErrors
 };
