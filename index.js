78a59df0af8c86ea69ff4ef39f6c31c174a742ac
Ciprian Lujeru <Ciprian.Lujeru@iquestgroup.com>
2017-03-10 14:57:05 +0200

    Extra functionality

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const acquisitionCtxClient = require('./lib/acquisitionCtxClient');
 const userProfileClient = require('./lib/userProfileClient');
 const clientErrors = require('./lib/statusErrors');
 const accessLicenceClient = require('./lib/accessLicenceClient');
+const kinesisClient = require('./lib/kinesisClient');
 
 module.exports={
 	myFTClient,
 	acquisitionCtxClient,
 	userProfileClient,
 	clientErrors,
+	kinesisClient,
 	accessLicenceClient
 };
