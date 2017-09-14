1683b06ce275563169afaf25cb81eacb58055cac
Tye, Thurston <Thurston.Tye@ft.com>
2017-09-14 12:29:54 +0100

    Call it as it is and start adding in some thing like test around this method  🐿 v2.5.16

--- test/syncUserFollows.spec.js
+++ test/syncUserFollows.spec.js
@@ -14,19 +14,67 @@ const mockAPI = env.USE_MOCK_API;
 const expectOwnProperties = require('./helpers/expectExtensions').expectOwnProperties;
 const baseUrl = config.MYFT_API_URL;
 const extraParams = `?noEvent=${config.MYFT_NO_EVENT}&waitForPurge=${config.MYFT_WAIT_FOR_PURGE_ADD}`;
-const sync = proxies.sync;
+const syncUserFollowers = proxies.syncUserFollowers;
 
 const myftConst = config.myftClientConstants;
+const suppressLogs = false; //for local test if you want logs when test are run
 
 //TODO spec out tests for syncUserFollowers
-xdescribe('syncUserFollowers', () => {
-  const groupId = '00000000-0000-0000-0000-000000000123';
-    const uuid = '00000000-0000-0000-0000-000000000001';
+describe.only('syncUserFollowers', () => {
+  // let logMessageStub;
+  // const logMessages = [];
 
-  it('should do something', (done)=> {
+  before(done => {
+    // if(suppressLogs) {
+    //   logMessageStub = sinon.stub(logger, 'log').callsFake((...params) => {
+    //     logMessages.push(params);
+    //   });
+    // }
 
-    return sync.userFollowers(groupId, uuid);
     done();
   });
 
+  afterEach(done => {
+      nock.cleanAll();
+      done();
+  });
+
+  after(done => {
+
+
+    // if(suppressLogs) {
+    //   logMessageStub.restore();
+    // }
+    done();
+  });
+
+  const groupId = '00000000-0000-0000-0000-000000000123';
+  const uuid = '00000000-0000-0000-0000-000000000001';
+
+  //Happy empyty path
+  it('should return status synchronisationIgnored and reason noGroupConceptsToFollow in object for no topics', (done)=> {
+
+    nock(baseUrl)
+      .get(`/group/${groupId}/followed/concept`)
+      .query(true)
+      .reply(200, () => []);
+
+    syncUserFollowers(groupId, uuid).then(res => {
+      console.log(res);
+      expect(res).to.be.an('object');
+      expect(res).to.have.deep.property('user.status', 'synchronisationIgnored');
+      expect(res).to.have.deep.property('user.reason', 'noGroupConceptsToFollow');
+      done();
+    }).catch(done);
+
+  });
+
+  xit('should return synchronisationIgnored if there are no topics to follow', (done)=> {
+
+    syncUserFollowers(groupId, uuid).then(res => {
+      console.log(res);
+      done();
+    }).catch(done);
+
+  });
 });
